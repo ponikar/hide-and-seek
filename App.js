0c69ea1269da5e239c4c9d491929e2c06b990ced
Darshan Ponikar <drsofficial3k@gmail.com>
2020-12-03 23:51:46 +0530

    Splash Screen working fine, Testing done!

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+const App = () => {
 
-const App = () => (
-    <View />
-);
+  useEffect(() => {
+   SplashScreen.hide();
+  }, []);
+
+  return <View />;
+}
 
 export default App;
--- android/app/src/main/java/com/hide/MainActivity.java
+++ android/app/src/main/java/com/hide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hide;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
+import android.os.Bundle; 
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,5 +13,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Hide";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
