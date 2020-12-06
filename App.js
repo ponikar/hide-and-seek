5153cdfa0cc94af9cc548077acaad0c79c3d1bc9
Darshan Ponikar <drsofficial3k@gmail.com>
2020-12-06 17:04:28 +0530

    working with common components

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
+import PrimaryButton from './src/Components/Primary_Button/primary-button.component';
+import PrimaryLabel from './src/Components/Primary_Label/primary-label.component';
 const App = () => {
 
   useEffect(() => {
@@ -9,7 +11,11 @@ const App = () => {
   }, []);
 
   return <View>
-    <Text>Hello world!</Text>
+    <PrimaryLabel large label="Login with Email" />
+    <PrimaryLabel label="Login with Email" />
+    <PrimaryButton />
+    <PrimaryButton />
+
   </View>;
 }
 
